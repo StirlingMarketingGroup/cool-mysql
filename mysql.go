f0514e77ce08b579255ff9f47949ca4560e4600e
Brian Leishman <b@rian.gg>
2020-10-19 23:03:02 -0400

    first commit

---
+++ mysql.go
@@ -0,0 +1,88 @@
+package mysql
+
+import (
+	"database/sql"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// Database is a cool MySQL connection
+type Database struct {
+	Writes *sql.DB
+	Reads  *sql.DB
+
+	Logging bool
+	Log     []string
+}
+
+// New creates a new Database
+func New(wUser, wPass, wSchema, wHost string, wPort int,
+	rUser, rPass, rSchema, rHost string, rPort int,
+	timeZone *time.Location) (db *Database, err error) {
+	writes := mysql.NewConfig()
+	writes.User = wUser
+	writes.Passwd = wUser
+	writes.DBName = wSchema
+	writes.Net = "tcp"
+	writes.Addr = net.JoinHostPort(wHost, strconv.Itoa(wPort))
+	writes.Loc = timeZone
+	writes.ParseTime = true
+	writes.InterpolateParams = true
+
+	reads := mysql.NewConfig()
+	reads.User = rUser
+	reads.Passwd = rUser
+	reads.DBName = rSchema
+	reads.Net = "tcp"
+	reads.Addr = net.JoinHostPort(rHost, strconv.Itoa(rPort))
+	reads.Loc = timeZone
+	reads.ParseTime = true
+	reads.InterpolateParams = true
+
+	return NewFromDSN(writes.FormatDSN(), reads.FormatDSN())
+}
+
+// NewFromDSN creates a new Database from config
+// DSN strings for both connections
+func NewFromDSN(writes, reads string) (db *Database, err error) {
+	db.Writes, err = sql.Open("mysql", writes)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Writes.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	if reads != writes {
+		db.Reads, err = sql.Open("mysql", reads)
+		if err != nil {
+			return nil, err
+		}
+
+		err = db.Reads.Ping()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		db.Reads = db.Writes
+	}
+
+	return
+}
+
+// Select selects one or more rows into the
+// struct or chan of structs in the destination
+func (db *Database) Select(dest interface{}, query string, cache time.Duration) chan error {
+	errCh := make(chan error)
+
+	go func() {
+
+	}()
+
+	return errCh
+}
