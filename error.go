d4dee1496f26877b4725d0d50ab85aa1e7756e9c
Ian Shearer <iangcshearer@gmail.com>
2021-01-08 11:17:29 -0500

    query error logging length

--- error.go
+++ error.go
@@ -15,6 +15,14 @@ type Error struct {
 	Params        Params
 }
 
+// QueryErrorLoggingLength is the size of the query
+// characters that are logged when an error occurs
+var QueryErrorLoggingLength = 1 << 11 // 2kB
+
 func (v Error) Error() string {
+	if len(v.ReplacedQuery) > QueryErrorLoggingLength {
+		half := QueryErrorLoggingLength >> 1
+		v.ReplacedQuery = v.ReplacedQuery[:half] + fmt.Sprintf("\n/* %d characters hidden */\n", len(v.ReplacedQuery)-QueryErrorLoggingLength) + v.ReplacedQuery[len(v.ReplacedQuery)-half:]
+	}
 	return fmt.Sprintf("%s\n\nquery len:\n%d\n\nquery:\n%s\n\nparams:\n%s", v.Err.Error(), len(v.ReplacedQuery), v.ReplacedQuery, spew.Sdump(v.Params))
 }
