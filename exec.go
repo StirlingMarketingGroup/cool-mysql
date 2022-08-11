fbc29dac4ef7b0b4b35737fb5cc44854ec430527
Brian Leishman <b@rian.gg>
2022-08-11 14:24:09 -0400

    insert uniquely same conn as insert

--- exec.go
+++ exec.go
@@ -12,12 +12,13 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
-type Executor interface {
+type executor interface {
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	querier
 }
 
 // exec executes a query and nothing more
-func (db *Database) exec(ex Executor, ctx context.Context, query string, params ...Params) (sql.Result, error) {
+func (db *Database) exec(ex executor, ctx context.Context, query string, params ...Params) (sql.Result, error) {
 	replacedQuery, mergedParams := ReplaceParams(query, params...)
 	if db.die {
 		fmt.Println(replacedQuery)
